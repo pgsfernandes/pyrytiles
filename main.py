8775bd8f6a7caafe3473a17baf60dd7688acb28e
pgsfernandes <pedrogsilvafernandes@gmail.com>
2026-04-21 16:28:36 +0200

    updated toml

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-#from pyrytiles import compile_primary, compile_secondary, decompile_tileset
 import pyrytiles
 
 # To load a directory it is sometimes more convenient to do: os.path.expandvars("$HOME/Documents/...")
@@ -9,5 +8,5 @@ pyrytiles.compile_primary(path="Test/EmeraldGeneral/Decompiled",out_dir="Test/Em
 pyrytiles.compile_secondary(path="Test/Slateport/DecompiledEmeraldOriginal",out_dir="Test/Slateport/Recompiled",path_primary="Test/EmeraldGeneral/Recompiled",optimal=False, triple_layer=False)
 
 #Decompile Emerald's primary tileset and Slateport's tileset that were compiled with triple-layer
-#decompile_tileset(primary_path="Test/EmeraldGeneral/RecompiledTriple", out_dir="Test/EmeraldGeneral/DecompiledTriple", triple_layer=True)
-#decompile_tileset(primary_path="Test/EmeraldGeneral/RecompiledTriple", secondary_path="Test/Slateport/RecompiledTriple", out_dir="Test/Slateport/DecompiledTriple", triple_layer=True)
+#pyrytiles.decompile_tileset(primary_path="Test/EmeraldGeneral/RecompiledTriple", out_dir="Test/EmeraldGeneral/DecompiledTriple", triple_layer=True)
+#pyrytiles.decompile_tileset(primary_path="Test/EmeraldGeneral/RecompiledTriple", secondary_path="Test/Slateport/RecompiledTriple", out_dir="Test/Slateport/DecompiledTriple", triple_layer=True)
